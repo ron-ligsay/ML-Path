5ed68bcd6605dbff7539a368b099ccf59508ec65
ron-ligsay <akyligsay16@gmail.com>
2023-09-08 12:25:02 +0800

    keras code note

---
+++ src/deep_learning/02_code_notes_keras_deeplearning.py
@@ -0,0 +1,13 @@
+
+# Model Specification
+import numpy as np
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+
+predeictors = np.loadtxt('predictors_data.csv', delimiter=',')
+n_cols = predictors.shape[1]
+
+model = Sequential()
+model.add(Dense(100, activation='relu', input_shape=(n_cols,)))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(1))
