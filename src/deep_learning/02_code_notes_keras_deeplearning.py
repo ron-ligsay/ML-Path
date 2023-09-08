66f68d0c13769685d950afdb39a83a151e42f309
ron-ligsay <akyligsay16@gmail.com>
2023-09-08 12:26:18 +0800

    sequential

--- src/deep_learning/02_code_notes_keras_deeplearning.py
+++ src/deep_learning/02_code_notes_keras_deeplearning.py
@@ -7,7 +7,9 @@ from tensorflow.keras.models import Sequential
 predeictors = np.loadtxt('predictors_data.csv', delimiter=',')
 n_cols = predictors.shape[1]
 
+# n_cols = number of predictors , number of nodes in the input later
 model = Sequential()
 model.add(Dense(100, activation='relu', input_shape=(n_cols,)))
 model.add(Dense(100, activation='relu'))
 model.add(Dense(1))
+
