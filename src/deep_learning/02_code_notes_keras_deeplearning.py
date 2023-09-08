fe5c375bd146a40d0f81abc3e90c90ee96c96179
ron-ligsay <akyligsay16@gmail.com>
2023-09-08 15:33:40 +0800

    save, load, use

--- src/deep_learning/02_code_notes_keras_deeplearning.py
+++ src/deep_learning/02_code_notes_keras_deeplearning.py
@@ -34,3 +34,9 @@ model.add(Dense(2, activation='softmax'))
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 model.fit(predictors, target)
 
+# Saving, Reloading, and using your model
+from tensoflow.keras.models import load_model
+model.save('model_file.h5')
+my_model = load_model('my_model.h5')
+predictions = my_model.predict(data_to_predict_with)
+probability_true = predictions[:,1]
