3b04497b3b8766c00506b89516049e3b9d61d43e
ron-ligsay <akyligsay16@gmail.com>
2023-09-13 15:23:54 +0800

    callbacks

--- src/deep_learning/04_performance.md
+++ src/deep_learning/04_performance.md
@@ -5,3 +5,25 @@ reference: Chapter 3 of Introduction to Deep Learning with Keras from Datacamp
 **loss curves** - plots of loss function over time
 
 **accuracy curves** - plots of accuracy over time
+
+### Activation Functions
+a = sum of **inputs*weights** + 
+activation function: f(a) = y
+
+**activation functions**
+* sigmoid - squashes numbers to range of 0 to 1
+* tanh - squashes numbers to range of -1 to 
+* relu - returns 0 if input is negative, otherwise returns input, range of 0 to infinity
+* Leaky relu - returns 0.01 if input is negative, otherwise returns input, range of -infinity to infinity
+
+
+**effects of activation functions**
+* sigmoid - creates a boundary in a linearly separable dataset
+* tanh - smooth close to rounded edges
+* relu - creates a sharper boundary
+* Leaky relu - creates a similar boundaries with rely but with more corners
+
+Comaparing activation functions
+it is important to set a seed so that the results are reproducible
+
+note: the sigmoid and tanh both take values close to -1 for big negative numbers.
--- src/deep_learning/04_performance.py
+++ src/deep_learning/04_performance.py
@@ -78,3 +78,72 @@ for size in training_sizes:
     
 # Plot train vs test accuracies
 plot_results(train_accs, test_accs)
+
+
+
+# Activation Functions
+# Set a random  seed
+np.random.seed(1)
+
+# Return a new model with the given activation
+def get_model(act_function):
+   model = Sequential()
+   model.add(Dense(4, input_shape = (2,), activation = act_function))
+   model.add(Dense(1, activation = 'sigmoid'))
+   return model
+
+# Activation functions to try out
+activations = ['relu', 'sigmoid', 'tanh']
+
+# Dictionary to store results
+activation_results = {}
+for funct in activations:
+   model = get_model(act_function=funct)
+   history = model.fit(X_train, y_train, validation_data = (X_test, y_test), epochs = 100, verbose = 0)
+   activation_results[funct] = history
+
+
+# Comparing activation functions
+
+# Extract val_loss history of each activation function
+val_loss_per_funct = {k:v.history['val_loss'] for k,v in activation_results.items()}
+
+# turn the dictionary into a pandas dataframe
+val_loss_curves = pd.DataFrame(val_loss_per_funct)
+
+# plot the curves
+val_loss_curves.plot(title = 'Validation Loss per Activation Function')
+
+
+# Exercise
+# Activation functions to try
+activations = ['relu', 'leaky_relu', 'sigmoid', 'tanh']
+
+# Loop over the activation functions
+activation_results = {}
+
+for act in activations:
+  # Get a new model with the current activation
+  model = get_model(act)
+  # Fit the model and store the history results
+  h_callback = model.fit(X_train, y_train, validation_data = (X_test,y_test), epochs = 20, verbose = 0)
+  activation_results[act] = h_callback
+
+
+# Exercise
+# Comapring activation function
+# Create a dataframe from val_loss_per_function
+val_loss= pd.DataFrame(val_loss_per_function)
+
+# Call plot on the dataframe
+val_loss.plot()
+plt.show()
+
+# Create a dataframe from val_acc_per_function
+val_acc = pd.DataFrame(val_acc_per_function)
+
+# Call plot on the dataframe
+val_acc.plot()
+plt.show()
+
+# 
