a1e9cb5f494023ec8f54cb7664e0d477660b315b
ron-ligsay <akyligsay16@gmail.com>
2023-09-13 15:49:17 +0800

    batch size a nd normalization

--- src/deep_learning/04_performance.md
+++ src/deep_learning/04_performance.md
@@ -27,3 +27,20 @@ Comaparing activation functions
 it is important to set a seed so that the results are reproducible
 
 note: the sigmoid and tanh both take values close to -1 for big negative numbers.
+
+
+### Batch size and Batch normalization
+The network is fed with 3 mini-batches, 1 epoch = 3 weight , 1 for each mini-batch
+
+**mini-batches**
+Advantages
+* networks train faster (more wieght updaates in same amount of time)
+* requires less memory (less data to store), can train on larger datasets
+* noise can help the network avoid local minima, reach a lower error
+Disadvantages
+* more iterations need to be run
+* need to be adjusted, we need to find a good batch size
+
+**normalization (standardization)**
+(data - mean) / standard 
+standardized sets the mean to 0 and the standard deviation to 1
--- src/deep_learning/04_performance.py
+++ src/deep_learning/04_performance.py
@@ -145,5 +145,66 @@ val_acc = pd.DataFrame(val_acc_per_function)
 # Call plot on the dataframe
 val_acc.plot()
 plt.show()
- 
-# 
+
+# batch size and batch normalization
+
+# batch size in Keras
+# fitting an already build and compiled model
+model.fit(X_train, y_train, epochs=100, batch_size=128)
+
+# batch normalization in keras
+# import BatchNormalization from keras layers
+from tensorflow.keras.layers import BatchNormalization
+
+# instantiate a sequential model
+model = Sequential()
+# add an input layer
+model.add(Dense(3, input_shape=(2,), activation='relu'))
+# add batch normalization layer
+model.add(BatchNormalization())
+# add an output layer
+model.add(Dense(1, activation='sigmoid'))
+
+# Exercise
+# Cchanging batch sizes
+# Get a fresh new model with get_model
+model = get_model()
+
+# Train your model for 5 epochs with a batch size of 1
+model.fit(X_train, y_train, epochs=5, batch_size=1)
+print("\n The accuracy when using a batch of size 1 is: ",
+      model.evaluate(X_test, y_test)[1])
+
+# using the whole training set as batch size
+model = get_model()
+
+# Fit your model for 5 epochs with a batch of size the training set
+model.fit(X_train, y_train, epochs=5, batch_size=X_train.shape[0])
+print("\n The accuracy when using the whole training set as batch-size was: ",
+      model.evaluate(X_test, y_test)[1])
+
+# Batch normalizing a familiar model, digital dataset
+# Import batch normalization from keras layers
+from tensorflow.keras.layers import BatchNormalization
+
+# Build your deep network
+batchnorm_model = Sequential()
+batchnorm_model.add(Dense(50, input_shape=(64,), activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(50, activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(50, activation='relu', kernel_initializer='normal'))
+batchnorm_model.add(BatchNormalization())
+batchnorm_model.add(Dense(10, activation='softmax', kernel_initializer='normal'))
+
+# Compile your model with sgd
+batchnorm_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train your standard model, storing its history callback
+h1_callback = standard_model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs=10, verbose=0)
+
+# Train the batch normalized model you recently built, store its history callback
+h2_callback = batchnorm_model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs=10, verbose=0)
+
+# Call compare_histories_acc passing in both model histories
+compare_histories_acc(h1_callback, h2_callback)
