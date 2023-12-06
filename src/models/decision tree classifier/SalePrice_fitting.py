ad15efff85211bf7019c7403f2c6878bbce6e4e7
ron-ligsay <akyligsay16@gmail.com>
2023-12-07 00:17:15 +0800

    fittings

---
+++ src/models/decision tree classifier/SalePrice_fitting.py
@@ -0,0 +1,76 @@
+from sklearn.metrics import mean_absolute_error
+from sklearn.tree import DecisionTreeRegressor
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+# compare MAE with differing values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000]:
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes: %d  \t\t Mean Absolute Error:  %d" %(max_leaf_nodes, my_mae))
+
+
+# Max leaf nodes: 5  		 Mean Absolute Error:  347380
+# Max leaf nodes: 50  		 Mean Absolute Error:  258171
+# Max leaf nodes: 500  		 Mean Absolute Error:  243495
+# Max leaf nodes: 5000       Mean Absolute Error:  254983
+
+
+# Of the options listed, 500 is the optimal number of leaves.
+
+# Conclusion
+# Here's the takeaway: Models can suffer from either:
+
+# Overfitting: capturing spurious patterns that won't recur in the future, leading to less accurate predictions, or
+# Underfitting: failing to capture relevant patterns, again leading to less accurate predictions.
+# We use validation data, which isn't used in model training, to measure a candidate model's accuracy. 
+# This lets us try many candidate models and keep the best one.
+
+
+# Code you have previously used to load data
+import pandas as pd
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+
+
+# Path of the file to read
+iowa_file_path = '../input/home-data-for-ml-course/train.csv'
+
+home_data = pd.read_csv(iowa_file_path)
+# Create target object and call it y
+y = home_data.SalePrice
+# Create X
+features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
+X = home_data[features]
+
+# Split into validation and training data
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+
+# Specify Model
+iowa_model = DecisionTreeRegressor(random_state=1)
+# Fit Model
+iowa_model.fit(train_X, train_y)
+
+# Make validation predictions and calculate mean absolute error
+val_predictions = iowa_model.predict(val_X)
+val_mae = mean_absolute_error(val_predictions, val_y)
+print("Validation MAE: {:,.0f}".format(val_mae))
+
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex5 import *
+print("\nSetup complete")
+
+
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
